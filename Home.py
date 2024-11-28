0705b862f29a8ed3f71f65727adc65ead4a4ddd9
Aswa <65958724+Aswathaman-K@users.noreply.github.com>
2024-11-28 15:46:25 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="ChennaiPrice Pro", initial_sidebar_state='collapsed')
+st.set_page_config(page_title="ChennaiPrice Pro", initial_sidebar_state='collapsed', page_icon='🏠')
 st.sidebar.write("")
 st.sidebar.markdown("---")
 st.sidebar.title("About the Creator")
